91afd6e1562edecd6bc7332a6b8b412eee319b03
yuusufxb <youssefmouha98@gmail.com>
2026-05-10 18:49:16 +0100

    first -v / patient microservice class + repo

---
+++ patient-service/src/main/java/com/example/patientservice/service/PatientService.java
@@ -0,0 +1,9 @@
+package com.example.patientservice.service;
+
+import com.example.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientService {
+    private PatientRepository patientRepository ;
+}
