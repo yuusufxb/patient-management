1bb9df688e0601bea1a60f57235f247615ec9a5c
yuusufxb <youssefmouha98@gmail.com>
2026-05-15 01:23:05 +0100

    addind an exeption handler to hendle error responses

---
+++ patient-service/src/main/java/com/example/patientservice/exeption/GlobalExeptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.patientservice.exeption;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExeptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationExeption(MethodArgumentNotValidException ex){
+        Map<String,String> errors = new HashMap<>() ;
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(),error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
