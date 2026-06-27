3a40d07eef6ab08361409848c83f3aba5c138972
yuusufxb <youssefmouha98@gmail.com>
2026-06-27 03:14:31 +0100

    creating the doctore microservice

--- patient-service/src/main/java/com/example/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/example/patientservice/model/Patient.java
@@ -1,8 +1,10 @@
 package com.example.patientservice.model;
 
+import com.example.patientservice.enums.Gender;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 import java.util.UUID;
@@ -23,10 +25,19 @@ public class Patient {
     private String email ;
 
     @NotNull
+    @Column(unique = true )
+    private Integer phoneNumber ;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender ;
+
+    @NotNull
     private String address ;
 
     @NotNull
     private LocalDate dateOfBirth ;
+
+
 
     @NotNull
     private LocalDate registeredDate ;
@@ -58,6 +69,13 @@ public class Patient {
     public String getAddress() {
         return address;
     }
+
+    public Gender getGender() { return gender; }
+
+    public void setGender(Gender gender) { this.gender = gender; }
+    public Integer getPhoneNumber() { return phoneNumber; }
+
+    public void setPhoneNumber(Integer phoneNumber) { this.phoneNumber = phoneNumber; }
 
     public void setAddress(String address) {
         this.address = address;
