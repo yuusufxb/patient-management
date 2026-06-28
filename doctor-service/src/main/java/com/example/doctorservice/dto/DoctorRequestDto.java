950b89bb974fd5274556cb37320f91b9d74e3e44
yuusufxb <youssefmouha98@gmail.com>
2026-06-28 17:19:11 +0100

    creting the doctor response dto

---
+++ doctor-service/src/main/java/com/example/doctorservice/dto/DoctorRequestDto.java
@@ -0,0 +1,5 @@
+package com.example.doctorservice.dto;
+
+public class DoctorRequestDto {
+
+}
---
+++ doctor-service/src/main/java/com/example/doctorservice/dto/DoctorResposeDto.java
@@ -0,0 +1,54 @@
+package com.example.doctorservice.dto;
+
+import com.example.doctorservice.model.Doctor;
+
+import java.util.List;
+import java.util.UUID;
+
+public class DoctorResposeDto {
+    private UUID id ;
+    private String name ;
+    private String email ;
+    private Integer phone ;
+    private String specialisation ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getSpecialisation() {
+        return specialisation;
+    }
+
+    public void setSpecialisation(String specialisation) {
+        this.specialisation = specialisation;
+    }
+}
