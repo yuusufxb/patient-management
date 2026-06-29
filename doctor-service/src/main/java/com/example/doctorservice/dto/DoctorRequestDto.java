4b665c1cdaf648779b3de6356ad7409164b102ef
yuusufxb <youssefmouha98@gmail.com>
2026-06-29 23:19:49 +0100

    doctor request dto created ! with it's validation annotatios

--- doctor-service/src/main/java/com/example/doctorservice/dto/DoctorRequestDto.java
+++ doctor-service/src/main/java/com/example/doctorservice/dto/DoctorRequestDto.java
@@ -1,5 +1,72 @@
 package com.example.doctorservice.dto;
 
+import com.example.doctorservice.enums.Specialisation;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
 public class DoctorRequestDto {
 
+    @NotBlank(message = "name is required")
+    private String name ;
+
+    @NotBlank(message = "email is required")
+    @Email(message = "email is not valid")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "phone is required")
+    @Pattern(regexp = "\\d{10}" , message = "phone number must contain 10 digits")
+    private String phone ;
+
+    @NotBlank(message = "license number is required")
+    private String licenseNumber ;
+
+    @NotNull(message = "specialisation is required")
+    private Specialisation specialisation ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLicenseNumber() {
+        return licenseNumber;
+    }
+
+    public void setLicenseNumber(String licenseNumber) {
+        this.licenseNumber = licenseNumber;
+    }
+
+    public Specialisation getSpecialisation() {
+        return specialisation;
+    }
+
+    public void setSpecialisation(Specialisation specialisation) {
+        this.specialisation = specialisation;
+    }
 }
