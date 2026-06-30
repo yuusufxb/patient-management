3e37f82ffcca31bfc189a0fcd5f3548e7fe8d462
yuusufxb <youssefmouha98@gmail.com>
2026-06-30 18:21:46 +0100

    create toModel method in doctor mapper

--- back-end/doctor-service/src/main/java/com/example/doctorservice/mapper/DoctorMapper.java
+++ back-end/doctor-service/src/main/java/com/example/doctorservice/mapper/DoctorMapper.java
@@ -1,8 +1,10 @@
 package com.example.doctorservice.mapper;
 
+import com.example.doctorservice.dto.DoctorRequestDto;
 import com.example.doctorservice.dto.DoctorResposeDto;
 import com.example.doctorservice.model.Doctor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class DoctorMapper {
@@ -14,5 +16,15 @@ public class DoctorMapper {
         doctorResposeDto.setPhone(doctor.getPhone());
         doctorResposeDto.setSpecialisation(doctor.getSpecialisation().toString());
         return doctorResposeDto ;
+    }
+    public static Doctor toModule(DoctorRequestDto doctorRequestDto){
+        Doctor newDoctor = new Doctor();
+        newDoctor.setName(doctorRequestDto.getName());
+        newDoctor.setEmail(doctorRequestDto.getEmail());
+        newDoctor.setDateOfBirth(doctorRequestDto.getDateOfBirth());
+        newDoctor.setPhone(doctorRequestDto.getPhone());
+        newDoctor.setLicenseNumber(doctorRequestDto.getLicenseNumber());
+        newDoctor.setSpecialisation(doctorRequestDto.getSpecialisation());
+        return newDoctor ;
     }
 }
